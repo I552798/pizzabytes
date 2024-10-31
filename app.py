f92381b6240f46a8bc21d909e0a7d208cbf0f95e
KaterinaSSlavova <k.slavova@student.fontys.nl>
2024-10-31 09:37:15 +0100

    changing the names of the pics

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template, url_for
 app=Flask(__name__)
 
-pizzas = {
+menu = {
     1: {"name": "Pepperoni", "price": 13.10, "description": "Hand-tossed dough, house-made tomato sauce, mozzarella cheese, and savory pepperoni."},
     2: {"name": "Margherita", "price": 12.95, "description": "Hand-tossed dough, tomato sauce, fresh mozzarella, vine-ripened tomatoes, fresh basil, and extra virgin olive oil."},
     3: {"name": "Prosciutto", "price": 16.70, "description": "Hand-tossed dough, tomato sauce, fresh mozzarella, thinly sliced prosciutto, arugula, shaved Parmesan, and extra virgin olive oil."},
@@ -11,8 +11,8 @@ pizzas = {
 orders = []
 
 @app.route('/')
-def display():
-    return redirect(url_for('order_customer'))
+def menu_page():
+    return render_template('customer_order_page.html', menu=menu)
 
 @app.route('/menu', methods=['GET', 'POST'])
 def order_customer():
